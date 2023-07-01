f0d43d461991d7696d20871b0d2de7c71575e910
GlacialFlower7 <138139884+GlacialFlower7@users.noreply.github.com>
2023-07-01 18:08:47 +0200

    scraper script initial commit

---
+++ scraper.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
+
+driver = webdriver.Chrome(options=options)
+url = 'https://galxe.com/Linea/campaign/GCfkeUSkbD'
+
+i = 0
+while i < 100:
+	driver.get(url)
+
+	time.sleep(7)
+	try:
+	    #points = driver.find_element(By.CLASS_NAME, "All participants")
+	    q = driver.find_element(By.CLASS_NAME, 'participants-count')
+	    #q = driver.find_element(By.CLASS_NAME, 'content')
+	    print()
+	    #print('test successful')
+	    print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+	    print(q.text)
+	    #print(q.is_displayed())
+	    print()
+	except NoSuchElementException:
+	    print()
+	    print('No participant block displayed yet T_T ')
+	    print()
+	s = str(q.text)
+	N = s[18:-1]
+	with open('./out.tab', 'a') as f:
+		f.write(str(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + '\t' + str(N) + '\n'))
+
+	## Comment out for an infinite loop:
+	#i+=1
+	time.sleep(1200)
+
+driver.quit()
