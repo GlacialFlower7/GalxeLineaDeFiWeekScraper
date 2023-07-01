0c255675388ae11aaca5d5afd6dd03135699a733
GlacialFlower7 <138139884+GlacialFlower7@users.noreply.github.com>
2023-07-01 18:10:37 +0200

    removed unused libraries

--- scraper.py
+++ scraper.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 import time
-import pandas as pd
 
 options = Options()
 options.headless = True
